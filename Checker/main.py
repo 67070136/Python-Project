8efd0e683a7a654060ee303e5476b11a744ac2ac
pprakobk <phuthis.p@gmail.com>
2024-10-31 19:18:02 +0700

    SFX, Logo, New Font

--- Checker/main.py
+++ Checker/main.py
@@ -20,15 +20,25 @@ pygame.display.set_caption('Checkers')  # ชื่อเวลาเปิดwi
 icon = pygame.image.load('assets/logo.png')
 pygame.display.set_icon(icon)
 
+# Menu logo
+Menu_logo_image = pygame.image.load('assets/logo.png')
+Menu_logo_image = pygame.transform.scale(Menu_logo_image, (200, 200))
+Menu_logo_rect = Menu_logo_image.get_rect(midtop=(WIDTH // 2, 80))
+
 # Load the PNG start button image
 start_button_image = pygame.image.load('assets/start_button.png')  # Replace with the actual image path
 start_button_image = pygame.transform.scale(start_button_image, (150, 50))  # Resize the button
 start_button_rect = start_button_image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 
 # Title font and text
-title_font = pygame.font.Font(None, 64)  # Font size for title
+title_font = pygame.font.Font('assets/upheavtt.ttf', 64)  # Font size for title
 title_text = title_font.render("Checkers Game", True, (0, 0, 0))  # Black title text
 title_rect = title_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 100))  # Position above the button
+
+#SFX 
+Red_win_sfx = pygame.mixer.Sound('assets/Red wins.mp3') 
+White_win_sfx = pygame.mixer.Sound('assets/White wins.mp3')
+click_sound = pygame.mixer.Sound("assets/Pieces move.mp3")
 
 def get_row_col_from_mouse(pos):
     x, y = pos
@@ -44,6 +54,9 @@ def start_screen():
         # Draw title and button
         WIN.blit(title_text, title_rect)
         WIN.blit(start_button_image, start_button_rect)
+
+        # Draw Logo
+        WIN.blit(Menu_logo_image, Menu_logo_rect)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -70,8 +83,10 @@ def main():
         if winner is not None:
             if winner == "WHITE":  # Check against string identifiers
                 WIN.blit(WHITE_WIN_IMG, WHITE_WIN_rect)
+                White_win_sfx.play()
             elif winner == "RED":  # Check against string identifiers
                 WIN.blit(RED_WIN_IMG, RED_WIN_rect)
+                Red_win_sfx.play()
             print(winner)
             
             pygame.display.update()
@@ -88,6 +103,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
+                click_sound.play()
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
                 game.select(row, col)
